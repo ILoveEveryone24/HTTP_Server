115c1bc01f04c95250fec05347e3e9577759bfe5
ILoveEveryone24 <ernestascizas2404@gmail.com>
2024-10-25 12:38:17 +0200

    http server

--- main.c
+++ main.c
@@ -5,8 +5,20 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+#define PORT 8000
+
 int main(){
 	int server_socket, client_socket;
+	struct sockaddr_in server_addr, client_addr;
+	socklen_t addr_size = sizeof(client_addr);
+
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(PORT);
+
+	bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	listen(server_socket, 10);
 
 	return 0;
 }
