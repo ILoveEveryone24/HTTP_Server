dc672d9847bd0137780487981c58eecb1ddf2cf5
ILoveEveryone24 <ernestascizas2404@gmail.com>
2024-10-25 12:34:49 +0200

    http server

---
+++ http_server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+int main(){
+	int server_socket, client_socket;
+
+	return 0;
+}
