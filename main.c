6120d376c747fcac0fd25a6f5573e2ae643c2978
ILoveEveryone24 <ernestascizas2404@gmail.com>
2024-10-26 09:56:37 +0200

    http server

--- main.c
+++ main.c
@@ -5,20 +5,66 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define PORT 8000
+#define PORT 80
 
 int main(){
-	int server_socket, client_socket;
-	struct sockaddr_in server_addr, client_addr;
-	socklen_t addr_size = sizeof(client_addr);
-
+	int server_socket;
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(PORT);
+	if(server_socket < 0){
+		perror("Failed to create socket");	
+		return -1;
+	}
 
-	bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
-	listen(server_socket, 10);
+	struct sockaddr_in server_addr;
+	socklen_t server_addr_len = sizeof(server_addr);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+
+	if(bind(server_socket, (struct sockaddr *)&server_addr, server_addr_len) < 0){
+		perror("Failed to bind the socket");
+		close(server_socket);
+		return -1;
+	}
+
+	if(listen(server_socket, 3) < 0){
+		perror("Failed to listen to the socket");
+		close(server_socket);
+		return -1;
+	}
+
+	int accept_socket;
+	accept_socket = accept(server_socket, (struct sockaddr *)&server_addr, &server_addr_len);
+
+	if(accept_socket < 0){
+		perror("Failed to accept the socket");
+		close(accept_socket);
+		close(server_socket);
+		return -1;
+	}
+
+	int buffer[1024];
+	memset(buffer, 0, sizeof(buffer));
+	ssize_t bytes_read = read(accept_socket, buffer, sizeof(buffer));
+	if(bytes_read < 0){
+		perror("Failed to read data from socket");
+		close(accept_socket);
+		close(server_socket);
+		return -1;
+	}
+	printf("Request: \n%zd\n", bytes_read);
+
+	const char *response = "HTTP/1.1 200 OK\nContent-Length; 13\n\nHello, world!";
+	ssize_t bytes_write = write(accept_socket, response, strlen(response));
+	if(bytes_write < 0){
+		perror("Failed to write data to socket");
+		close(accept_socket);
+		close(server_socket);
+		return -1;
+	}
+
+	close(accept_socket);
+	close(server_socket);
 
 	return 0;
 }
